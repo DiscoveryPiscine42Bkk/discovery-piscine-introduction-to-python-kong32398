250d055a55df89a38fc5a4878c6ec6a1ccbc669a
kong32398 <68040043@kmitl.ac.th>
2025-06-17 12:19:29 +0000

    ex01

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,11 @@
+def upcase_it(text):
+    """Method that takes a string and returns it in uppercase"""
+    return text.upper()
+
+def main():
+    
+    result = upcase_it("hello")
+    print(result)
+
+if __name__ == "__main__":
+    main()
