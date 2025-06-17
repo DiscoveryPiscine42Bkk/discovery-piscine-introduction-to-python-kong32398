49f0512bf8437189b66257c3639c59c8c63c44dd
kong32398 <68040043@kmitl.ac.th>
2025-06-17 10:17:55 +0000

    ex02

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,4 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array2 = [value + 2 for value in original_array if value > 5]
+print("Original array:", original_array)
+print("New array:", new_array2)
