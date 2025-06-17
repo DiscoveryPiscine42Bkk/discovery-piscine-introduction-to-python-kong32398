3aa6e08125b0c3d509152c7f25a63973efdf5d52
kong32398 <68040043@kmitl.ac.th>
2025-06-17 10:08:28 +0000

    ex01

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,4 +1,4 @@
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = [4, 10, 11, 50, 10, 24, -10, 4]
-for value in original_array:
-  new_array.append(value + 2)
+new_array2 = [value + 2 for value in original_array]
+print("Original array:", original_array)
+print("New array:", new_array2)
